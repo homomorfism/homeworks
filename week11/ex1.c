ca2f5b2be3ad9dc86d48ab22d180195bdea7e77b
Your Name <you@example.com>
2020-10-27 15:39:28 +0300

    fix

--- week11/ex1.c
+++ week11/ex1.c
@@ -16,15 +16,19 @@ int main() {
     stat("ex1.txt", &buff);
     printf("Size of file: %d", buff.st_size);
 
+    char* line = "This is a good day";
+    ftruncate(des, strlen(line));
     char* file = (char*)mmap(NULL, buff.st_size,
          PROT_READ | PROT_WRITE | PROT_EXEC | PROT_NONE,
          MAP_SHARED | MAP_ANONYMOUS, des, 0);
 
     //file = "This is a good day";
-    strcpy(file, "This is a good day");
+    strcpy(file, line);
 
     printf("\n%s", file);
 
+    munmap(file, strlen(line));
     close(des);
+
     return 0;
 }
